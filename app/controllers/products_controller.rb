e9983ba96dc13a34e4336c9d47293c6aba080b84
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-17 11:33:01 -0400

    dynamic update action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,19 +9,23 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
-  def create 
-    product = Product.new(
-      name: params[:input_name],
-      price: params[:input_price],
-      description: params[:input_description],
-    ) 
-    product.save
-    render json: product.as_json
-  end
+  # def create 
+  #   product = Product.new(
+  #     name: params[:input_name],
+  #     price: params[:input_price],
+  #     description: params[:input_description],
+  #   ) 
+  #   product.save
+  #   render json: product.as_json
+  # end
 
   def update
-    # product_update = Product.find_by(params[:id])
-    render json: {message: "hello"}
+    product_update = Product.find_by(id: params[:id])
+    product_update.name = params[:input_name] || product_update.name
+    product_update.price = params[:input_price] || product_update.price
+    product_update.description = params[:input_description] || product_update.description
+    product_update.save
+    render json: product_update.as_json
   end
 
 end
