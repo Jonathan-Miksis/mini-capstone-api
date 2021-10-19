a488a1f38d37c1a3bb968197fa6c476600a52030
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-18 22:09:35 -0400

    create is_discontinued? method for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,24 +6,24 @@ class ProductsController < ApplicationController
   def show
     the_id = params[:id]
     product = Product.find_by(id: the_id)
+    render json: product.as_json(methods: [:is_discontinued?])
+  end
+
+  def create 
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+    ) 
+    product.save
     render json: product.as_json
   end
 
-  # def create 
-  #   product = Product.new(
-  #     name: params[:input_name],
-  #     price: params[:input_price],
-  #     description: params[:input_description],
-  #   ) 
-  #   product.save
-  #   render json: product.as_json
-  # end
-
   def update
     product_update = Product.find_by(id: params[:id])
-    product_update.name = params[:input_name] || product_update.name
-    product_update.price = params[:input_price] || product_update.price
-    product_update.description = params[:input_description] || product_update.description
+    product_update.name = params[:name] || product_update.name
+    product_update.price = params[:price] || product_update.price
+    product_update.description = params[:description] || product_update.description
     product_update.save
     render json: product_update.as_json
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+
+  def is_discontinued?
+    if price < 10
+      p "true"
+    else 
+      p "false"
+  end
+end
+
+
 end
