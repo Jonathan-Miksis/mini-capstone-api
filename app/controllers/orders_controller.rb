a6fea0892c3d04f270e5b6f3332d3c650840aaf4
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-26 20:23:38 -0400

    add authorization to show and index actions, only show users orders when they are logged in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,46 @@
 class OrdersController < ApplicationController
 
   def index
-    order = Order.all
-    render json: order
+    if current_user
+      order = Order.all
+      render json: order
+    else
+      render json: {message: "You must be logged in to see the catalog."}
+    end
+
   end
 
   def show
-    order = Order.find_by(id: params[:id])
-    render json: order
+    if current_user
+      order = Order.find_by(id: params[:id])
+      if order.user_id == current_user.id
+        render json: order
+      else
+        render json: {message: "Sorry, no orders found."}
+      end
+    else
+      render json: {message: "You must be logged in to see the catalog."}
+    end
+
   end
 
   def create
-    order = Order.new(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: params[:params],
-      tax: params[:tax],
-      total: params[:total]
-    )
-    order.save
-    render json: order
+    if current_user
+      p current_user
+      order = Order.new(
+        user_id: params[:user_id],
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        subtotal: product.price,
+        tax: params[:tax],
+        total: params[:total],
+        user_id: current_user.id
+      )
+      # order.save
+      render json: order
+    else
+      render json: {message: "You must be logged in to make an order."}
+    end
   end
 
 end
