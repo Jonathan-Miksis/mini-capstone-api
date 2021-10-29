bc2d6f3c2df9cac669fa948b50dbb1ca600ec364
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-28 21:13:42 -0400

    add category_products table and many-to-many associations, link all tables together

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
   
   # validates :name, presence: { message: "You must have a name for the product"}
   # validates :price, numericality: { greater_than: 0 }
