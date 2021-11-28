92f69ae1d04221227a3a2e194dc61a7429799a4b
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-28 13:07:46 -0500

    hardcode seeds files to test production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 10.times do
-  new_product = Product.new(name: Faker::Beer.name, price: Faker::Number.decimal(l_digits: 2, r_digits: 2), description: Faker::Beer.style)
+  new_product = Product.new(name: "Budweisser" price: 5.99, description: "cheaper beer"
   new_product.save
 end
