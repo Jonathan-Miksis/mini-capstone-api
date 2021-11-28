56dc51bd2eef6be206f279d027233cc6044fab06
Jonathan-Miksis <jmiksis13@gmail.com>
2021-11-28 13:09:48 -0500

    update syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 10.times do
-  new_product = Product.new(name: "Budweisser" price: 5.99, description: "cheaper beer"
+  new_product = Product.new(name: "Budweisser", price: 5.99, description: "cheaper beer")
   new_product.save
 end
